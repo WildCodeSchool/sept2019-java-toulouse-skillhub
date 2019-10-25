0ea473d6e792c5d4237bb3d4848033bfbe408ba8
Nabil <benweb31@gmail.com>
2019-10-25 11:55:38 +0200

    retrait de la scrollbar en mobile, ajout des images

--- src/main/java/com/wildcodeschool/skillhub/controller/MainController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/MainController.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.skillhub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
