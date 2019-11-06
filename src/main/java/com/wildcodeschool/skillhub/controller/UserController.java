57f4c372c9f12e8e3e5559c9b7e73e597cdd3a42
soninlaw <jonsoninlaw@gmail.com>
2019-11-06 15:55:26 +0100

    Stash

--- src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.wildcodeschool.skillhub.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/connect")
+    public String connect(Model model, @RequestParam(name="username") String userName, @RequestParam(name="password") String password ) {
+
+    };
+
 }
--- src/main/java/com/wildcodeschool/skillhub/repository/UserRepository.java
+++ src/main/java/com/wildcodeschool/skillhub/repository/UserRepository.java
@@ -1,4 +1,35 @@
 package com.wildcodeschool.skillhub.repository;
 
+import com.wildcodeschool.skillhub.entity.Question;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
+
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/skillhub?serverTimezone=GMT";
+    private final static String DB_USER = "skillhub";
+    private final static String DB_PASSWORD = "5ki!!huB31";
+
+    public Long checkUser(String username, String password) {
+
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM user WHERE nickname = ? AND password = ?;"
+            );
+            statement.setString(1, username);
+            statement.setString(2, password);
+            List<Question> questions = new ArrayList<>();
+            ResultSet resultSet = statement.executeQuery();
+
+            }
+            return questions;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
