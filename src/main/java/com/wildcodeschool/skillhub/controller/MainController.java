ecdd0c90850e43c9f7f6dcaafa03df0ba78aa964
Nabil <benweb31@gmail.com>
2019-11-07 19:28:16 +0100

    Gestion de l'affichage du header en mode déconnecté sur la page A propos

--- src/main/java/com/wildcodeschool/skillhub/controller/MainController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/MainController.java
@@ -1,10 +1,23 @@
 package com.wildcodeschool.skillhub.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class MainController {
+
+    @GetMapping("/about")
+    public String about(Model model, HttpSession session) {
+        if (session.getAttribute("user") == null) {
+            model.addAttribute("connected", false);
+        } else {
+            model.addAttribute("connected", true);
+        }
+        return "about";
+    }
 
 }
