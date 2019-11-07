5c83b3c525bec6b7677a0749ac22cbfc2853d0fd
Nabil <benweb31@gmail.com>
2019-11-07 17:33:15 +0100

    lié le bon utilisateur de la session

--- src/main/java/com/wildcodeschool/skillhub/controller/QuestionController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.wildcodeschool.skillhub.controller;
 
+import com.wildcodeschool.skillhub.entity.User;
 import com.wildcodeschool.skillhub.repository.AnswerRepository;
 import com.wildcodeschool.skillhub.repository.QuestionRepository;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.sql.Date;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -35,10 +37,10 @@ public class QuestionController {
 
     @PostMapping("/answer")
     public String postAnswer(Model model, @RequestParam Long id,
-                               @RequestParam String body,
-                               @RequestParam Long userId) {
-
-        model.addAttribute("answer", answerRepository.saveAnswer(id, body, new Date(System.currentTimeMillis()), userId));
+                             @RequestParam String body, HttpSession session) {
+        User user = (User)session.getAttribute("user");
+        model.addAttribute("answer", answerRepository.saveAnswer(id, body, new Date(System.currentTimeMillis()),
+                user.getUserId()));
         model.addAttribute("question", questionRepository.findQuestion(id));
 
         return "redirect:question?id=" + id;
