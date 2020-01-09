f1beff535f56d01de22f90e307099acc9f16d8b0
bastienwcs <email>
2020-01-09 14:13:15 +0100

    add database

---
+++ database/skillhub.sql
@@ -0,0 +1,217 @@
+CREATE DATABASE  IF NOT EXISTS `skillhub` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `skillhub`;
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: skillhub
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.18.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `answer`
+--
+
+DROP TABLE IF EXISTS `answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answer` (
+  `id_answer` int(11) NOT NULL AUTO_INCREMENT,
+  `body` text COLLATE utf8_bin NOT NULL,
+  `date` date NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_question` int(11) NOT NULL,
+  PRIMARY KEY (`id_answer`),
+  KEY `answer_user_FK` (`id_user`),
+  KEY `answer_question0_FK` (`id_question`),
+  CONSTRAINT `answer_question0_FK` FOREIGN KEY (`id_question`) REFERENCES `question` (`id_question`),
+  CONSTRAINT `answer_user_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `answer`
+--
+
+LOCK TABLES `answer` WRITE;
+/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `picture`
+--
+
+DROP TABLE IF EXISTS `picture`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `picture` (
+  `id_picture` int(11) NOT NULL AUTO_INCREMENT,
+  `url` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_picture`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `picture`
+--
+
+LOCK TABLES `picture` WRITE;
+/*!40000 ALTER TABLE `picture` DISABLE KEYS */;
+INSERT INTO `picture` VALUES (1,'images/avatars/11.svg'),(2,'images/avatars/29.svg'),(3,'images/avatars/10.svg'),(4,'images/avatars/22.svg'),(5,'images/avatars/9.svg');
+/*!40000 ALTER TABLE `picture` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question` (
+  `id_question` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(280) COLLATE utf8_bin NOT NULL,
+  `body` text COLLATE utf8_bin NOT NULL,
+  `date` date NOT NULL,
+  `resolved` tinyint(1) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id_question`),
+  KEY `question_user_FK` (`id_user`),
+  CONSTRAINT `question_user_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question_skill`
+--
+
+DROP TABLE IF EXISTS `question_skill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question_skill` (
+  `id_question` int(11) NOT NULL,
+  `id_skill` int(11) NOT NULL,
+  PRIMARY KEY (`id_question`,`id_skill`),
+  KEY `question_skill_skill0_FK` (`id_skill`),
+  CONSTRAINT `question_skill_question_FK` FOREIGN KEY (`id_question`) REFERENCES `question` (`id_question`),
+  CONSTRAINT `question_skill_skill0_FK` FOREIGN KEY (`id_skill`) REFERENCES `skill` (`id_skill`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question_skill`
+--
+
+LOCK TABLES `question_skill` WRITE;
+/*!40000 ALTER TABLE `question_skill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `question_skill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `skill`
+--
+
+DROP TABLE IF EXISTS `skill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skill` (
+  `id_skill` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_skill`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skill`
+--
+
+LOCK TABLES `skill` WRITE;
+/*!40000 ALTER TABLE `skill` DISABLE KEYS */;
+INSERT INTO `skill` VALUES (1,'Java'),(2,'JavaScript'),(3,'Python'),(4,'Kotlin'),(5,'HTML/CSS'),(6,'Swift'),(7,'Ruby'),(8,'C#'),(9,'C++');
+/*!40000 ALTER TABLE `skill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(100) COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8_bin NOT NULL,
+  `id_picture` int(11) NOT NULL,
+  PRIMARY KEY (`id_user`),
+  KEY `user_picture_FK` (`id_picture`),
+  CONSTRAINT `user_picture_FK` FOREIGN KEY (`id_picture`) REFERENCES `picture` (`id_picture`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'bastienwcs','c5073612cb5aaec23c28c87215cd28c4ddde6e9fcfc30e4ade375be4bbaf0be2',5);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_skill`
+--
+
+DROP TABLE IF EXISTS `user_skill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_skill` (
+  `id_skill` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id_skill`,`id_user`),
+  KEY `user_skill_user0_FK` (`id_user`),
+  CONSTRAINT `user_skill_skill_FK` FOREIGN KEY (`id_skill`) REFERENCES `skill` (`id_skill`),
+  CONSTRAINT `user_skill_user0_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_skill`
+--
+
+LOCK TABLES `user_skill` WRITE;
+/*!40000 ALTER TABLE `user_skill` DISABLE KEYS */;
+INSERT INTO `user_skill` VALUES (1,1),(2,1),(5,1);
+/*!40000 ALTER TABLE `user_skill` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-09 14:12:17
